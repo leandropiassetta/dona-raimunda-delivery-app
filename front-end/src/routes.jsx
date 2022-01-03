39aee67e2c8db6b03f8c1cb107149dd981d6fdb7
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2022-01-03 20:00:57 -0300

    REQ1

---
+++ front-end/src/routes.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+
+function routes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Navigate replace to="/login" /> } />
+        <Route path="/login" element={ <Login /> } />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default routes;
