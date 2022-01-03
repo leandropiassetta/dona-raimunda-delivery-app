375347e5083ddad014beed6b358c679626210a80
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2022-01-03 20:20:40 -0300

    REQ2

--- front-end/src/pages/Login/index.jsx
+++ front-end/src/pages/Login/index.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
+import rockGlass from '../../images/rockGlass.svg';
 
 function Login() {
   return (
-    <div> oi</div>
+    <div>
+      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
+        Glass
+      </object>
+      <h4>Boteco do seu Raimundo</h4>
+      <form action="">
+        <input
+          placeholder="email@seuZeh.com.br"
+          type="text"
+          data-testid="common_login__input-email"
+        />
+        <input
+          placeholder="**********"
+          type="text"
+          data-testid="common_login__input-password"
+        />
+        <button
+          type="button"
+          data-testid="common_login__button-login"
+        >
+          LOGIN
+        </button>
+        <button
+          type="button"
+          data-testid="common_login__button-register"
+        >
+          Ainda não tenho conta
+        </button>
+      </form>
+    </div>
   );
 }
 
