22d58bcc414501bdbb853cea982138d28082d29c
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2022-01-05 15:25:49 -0300

    REQ12

--- front-end/src/routes.jsx
+++ front-end/src/routes.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
-import ClientProducts from './pages/clientProducts';
+import ClientProducts from './pages/ClientProducts';
 import Register from './pages/Register';
 
 function routes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={ <Navigate replace to="/login" /> } />
+        <Route exact path="/" element={ <Navigate replace to="/login" /> } />
         <Route path="/login" element={ <Login /> } />
         <Route path="/customer/products" element={ <ClientProducts /> } />
         <Route path="/register" element={ <Register /> } />
