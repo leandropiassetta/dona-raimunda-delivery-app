d151a3b5c5c247a6d9531aa99bab2814ecf7655b
Guilherme Nunes <guiicnunes@gmail.com>
2022-01-11 13:05:19 -0300

    FIX: Data-testids REQ 22 e 23, formato das datas e data-testid secreto

--- front-end/src/components/CustomerOrder.jsx
+++ front-end/src/components/CustomerOrder.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
-function CustomerOrder({ order, index }) {
+function CustomerOrder({ order }) {
   const history = useNavigate();
+  console.log(typeof order.total_price);
 
   return (
     <button onClick={ () => history(`/customer/orders/${order.id}`) } type="button">
       <div data-testid={ `customer_orders__element-order-id-${order.id}` }>
-        Pedido
-        { index + 1 }
+        { order.id }
       </div>
-      <div data-testid={ `customer_orders__element-delivery-status-${index}` }>
+      <div data-testid={ `customer_orders__element-delivery-status-${order.id}` }>
         { order.status }
       </div>
-      <div data-testid={ `customer_orders__element-order-date-${index}` }>
-        { order.sale_date }
+      <div data-testid={ `customer_orders__element-order-date-${order.id}` }>
+        { moment(order.sale_date).format('DD/MM/YYYY') }
       </div>
-      <div>
-        R$
-        { order.total_price.replace(('.', ',')) }
+      <div data-testid={ `customer_orders__element-card-price-${order.id}` }>
+        { order.total_price.replace('.', ',') }
       </div>
     </button>
   );
@@ -32,7 +32,6 @@ CustomerOrder.propTypes = {
     total_price: PropTypes.string,
     sale_date: PropTypes.string,
   }).isRequired,
-  index: PropTypes.number.isRequired,
 };
 
 export default CustomerOrder;
